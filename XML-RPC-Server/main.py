f68cf21f7bf5644e22c0493f52bf0c6d281d622c
Martin Wieckowski <tincho92wk@gmail.com>
2020-11-16 19:59:02 -0300

    Aguegué los cmdLoop al menu

--- XML-RPC-Server/main.py
+++ XML-RPC-Server/main.py
@@ -4,22 +4,27 @@ from classes.Comandos import Comandos
 print("\nBienvenido al proyecto de Programación Orientada a Objetos 2020 \n")
 print("¿Qué desea hacer?\n")
 print("Ver manual de instrucciones------------------1\nConectar con el robot------------------------2\nModo manual----------------------------------3\nEsperar insturcciones del cliente------------4 \nIniciar simulación de prueba-----------------5\n")
-res1 = int(input())
-if res1 == 1:
+res = int(input())
+if res == 1:
 	print("Sección de ayuda de comandos.Introduzca help para ver los comandos documentados\n")
 	Comandos.prompt = 'Esperando Instruccion: '
 	Comandos().cmdloop()
 
-if res1 == 2:
+if res == 2:
 	print("\nConectando con el robot...")
 	print("Conección exitosa")
 	#Conectar con la funcion Robot
-if res2 == 3:
-	print("Iniciando modo manual")
+	Comandos().cmdloop()
+if res == 3:
+	print("Iniciando modo manual...")
 	#Conectar con la funcion Robot
-if res2 == 4:
-		print("Lanzando servidor...\n")
-		obj = Comandos
-		servidor = XmlRpcServer()
-if res2 == 5:
-		print("Ejecutando archivo de prueba")
+	Comandos().cmdloop()
+if res == 4:
+	print("Lanzando servidor...\n")
+	obj = Comandos
+	servidor = XmlRpcServer()
+if res == 5:
+	print("Ejecutando archivo de prueba")
+	#Crear archivo de prueba, dar mensaje que finalizó la demostración, y volver con cmdLoop
+	Comandos().cmdloop()
+	
